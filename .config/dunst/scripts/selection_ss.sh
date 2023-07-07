e3dbc4a3affcc1162c3e802b12b99d994c74acee
Noriaela <22671423+Noriaela@users.noreply.github.com>
2023-07-07 02:33:36 -0700

    update

---
+++ .config/dunst/scripts/selection_ss.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the directory to save the screenshot
+save_dir="$HOME/Documents/screenshots"
+
+# Create the directory if it doesn't exist
+mkdir -p "$save_dir"
+
+# Capture the screenshot using maim
+screenshot_path="$save_dir/$(date +%s).png"
+maim -s "$screenshot_path"
+
+# Copy the screenshot path to the clipboard using xclip
+xclip -selection clipboard -t image/png < "$screenshot_path"
+
+# Display a notification using notify-send
+notification_text="Screenshot captured and saved to:\n$screenshot_path"
+notify-send -a "Screenshot Tool" "Screenshot Captured" "$notification_text"
+
